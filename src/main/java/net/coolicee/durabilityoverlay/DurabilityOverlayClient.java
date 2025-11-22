ea24b614b6de1bced3c98604d1da9729a72bda95
cool-icee <evanchen112233@gmail.com>
2025-11-22 17:53:57 +1100

    Initial commit

---
+++ src/main/java/net/coolicee/durabilityoverlay/DurabilityOverlayClient.java
@@ -0,0 +1,10 @@
+package net.coolicee.durabilityoverlay;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DurabilityOverlayClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
